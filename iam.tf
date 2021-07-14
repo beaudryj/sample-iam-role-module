d982a514184aba6bbbf75f41a6e5aa88e11de506
jbeaudry <joe.beaudry@slalom.com>
2021-07-14 11:56:59 -0400

    fixing policy name

--- iam.tf
+++ iam.tf
@@ -5,7 +5,7 @@ resource "aws_iam_role" "role" {
 
 resource "aws_iam_policy" "policy" {
   count       = var.create_policy ? 1 : 0
-  name        = var.role_name
+  name        = "${var.role_name}-policy"
   description = "Policy Created for ${var.role_name} via Terraform Module"
 
   policy = var.policy
